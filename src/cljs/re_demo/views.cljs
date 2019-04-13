e20c2a16c900faf6a9966e64eaf9877a5b8e23f1
Alexei Matveev <alexei.matveev@gmail.com>
2019-04-13 21:32:24 +0200

    AM: closing parens

--- src/cljs/re_demo/views.cljs
+++ src/cljs/re_demo/views.cljs
@@ -1,11 +1,9 @@
 (ns re-demo.views
   (:require
    [re-frame.core :as re-frame]
-   [re-demo.subs :as subs]
-   ))
+   [re-demo.subs :as subs]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]
-     ]))
+     [:h1 "Hello from " @name ", dear reader!"]]))
