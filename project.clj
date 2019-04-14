7bdcfeb89b03935a0aae48a3285fcc847e47015d
Alexei Matveev <alexei.matveev@gmail.com>
2019-04-14 21:59:23 +0200

    AM: +handler, effectively

--- project.clj
+++ project.clj
@@ -2,17 +2,28 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
-                 [re-frame "0.10.6"]]
+                 [re-frame "0.10.6"]
+                 ;; These were added together with the ring handler:
+                 [compojure "1.5.0"]
+                 [yogthos/config "0.8"]
+                 [ring "1.4.0"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :min-lein-version "2.5.3"
   :source-paths ["src/clj" "src/cljs"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-  :figwheel {:css-dirs ["resources/public/css"]}
+  :figwheel {:css-dirs ["resources/public/css"]
+             :ring-handler re-demo.handler/dev-handler}
   :profiles
   {:dev
    {:dependencies [[binaryage/devtools "0.9.10"]]
     :plugins      [[lein-figwheel "0.5.18"]]}
-   :prod {}}
+   :prod {}
+   :uberjar {:source-paths ["env/prod/clj"]
+             :omit-source  true
+             :main         re-demo.server
+             :aot          [re-demo.server]
+             :uberjar-name "re-demo.jar"
+             :prep-tasks   ["compile" ["cljsbuild" "once" "min"]]}}
   :cljsbuild
   {:builds
    [{:id           "dev"
@@ -27,6 +38,7 @@
                     :external-config      {:devtools/config {:features-to-install :all}}}}
     {:id           "min"
      :source-paths ["src/cljs"]
+     :jar true
      :compiler     {:main            re-demo.core
                     :output-to       "resources/public/js/compiled/app.js"
                     :optimizations   :advanced
